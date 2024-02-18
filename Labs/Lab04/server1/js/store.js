edc97cdd51be4116881295a2c87aa965fd29ea8c
Wallace <wtrinh1@my.bcit.ca>
2024-02-17 22:49:19 -0800

    resolving pathing issue

--- Labs/Lab04/server1/js/store.js
+++ Labs/Lab04/server1/js/store.js
@@ -2,7 +2,7 @@ function addDefinition() {
     const word = document.getElementById('word').value;
     const definition = document.getElementById('definition').value;
 
-    fetch('https://wallace-jerry4537-labs.vercel.app/api/definitions/', {
+    fetch('https://wallace-jerry4537-labs-lab04.vercel.app/api/definitions/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',

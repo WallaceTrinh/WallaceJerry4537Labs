fd3838de1081700fec968742bf16179205149adf
Wallace <wtrinh1@my.bcit.ca>
2024-03-09 15:45:03 -0800

    client.js still needs code

---
+++ Labs/Lab05/public/js/client.js
@@ -0,0 +1,19 @@
+
+
+// Client SQL stuff here
+// Client SQL stuff here
+// Client SQL stuff here
+// Client SQL stuff here
+// Client SQL stuff here
+// Client SQL stuff here
+
+
+
+
+
+
+
+
+// Event listeners for the buttons
+document.getElementById('pre-set-row').addEventListener('click', insertPresetRows);
+document.getElementById('submit-query').addEventListener('click', submitQuery);
